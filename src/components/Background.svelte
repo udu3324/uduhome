cca0f90629acd9c649d8308b9d380f5d75819b3c
udu3324 <47045986+udu3324@users.noreply.github.com>
2025-10-24 23:50:10 -0500

    add actual working bg i think

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -1,29 +1,73 @@
 <script>
-    import realisticCityGreen from '../assets/background/RealisticCityRenderGreenVersion.png'
-    import blenderScreenshot from '../assets/background/RealisticCityRender.png'
-    
-    let backgrounds = [
+    import { onMount } from 'svelte';
+
+    const photos = [
+        'vn_bodyofwater.JPG',
+        'vn_city.JPG',
+        'vn_watermountain.JPG',
+        'vn_beach.JPG',
+        'vn_streets.JPG',
+        'ca_recordstore.JPG',
+        'tx_bay.JPG',
+        'tx_bay2.JPG',
+        'ak_mountains.JPG',
+        'ak_mountains2.JPG',
+        'ca_cloudflare.JPG',
+        'ca_salesforce.JPG'
+    ]
+
+    const backgrounds = [
         {
-            obj: realisticCityGreen,
-            alt: "A city made in blender with a green filter on",
-            accent: "3"
+            file: "vn_bodyofwater.JPG",
+            tag: ["nature"],
+            alt: "body of water",
+            accent: "#79803b"
         },
         {
-            obj: blenderScreenshot,
-            alt: "A screenshot of a blender cycles project",
-            accent: "3"
+            file: "vn_city.JPG",
+            tag: ["urban"],
+            alt: "bright lights of city development",
+            accent: "#030e17"
+        },
+        {
+            file: "vn_watermountain.JPG",
+            tag: ["nature"],
+            alt: "mountain with water",
+            accent: "#aac0c6"
         }
     ]
-    
-    let background = Math.floor(Math.random() * backgrounds.length)
+
+    let background = -1
+    let imageURL = ''
+
+    onMount(async () => {
+        background = Math.floor(Math.random() * backgrounds.length)
+
+        document.documentElement.style.cssText = "--accent-color: " + backgrounds[background].accent
+
+        const img = await import(`../assets/background/${backgrounds[background].file}`)
+        imageURL = img.default.src
+
+        //console.log("url is " + backgrounds[background].file)
+        //console.log(imageURL)
+        //console.log(imageURL.src)
+
+        
+        // console.log("bg is", background)
+    })
 </script>
 
-<div class="fixed bg-amber-100 w-screen h-screen">
+<div class="fixed bg-black w-screen h-screen">
+    {#if background == -1}
+    <div class="w-screen h-screen bg-black"></div>
+    {:else}
     <img
-        src={backgrounds[background].obj.src}
-        id="bg-render" 
+        src={imageURL}
+        id="bg-render"
+        loading="lazy"
         alt={backgrounds[background].alt}
     />
+    {/if}
 </div>
 
 <style lang="postcss">

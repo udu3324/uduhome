cdeefca0a00f3eca26b1aee7756215fb5de420e0
udu3324 <47045986+udu3324@users.noreply.github.com>
2025-11-24 00:38:51 -0600

    add bunch of splashes

--- src/components/Splash.svelte
+++ src/components/Splash.svelte
@@ -6,19 +6,50 @@
     const imageArray = Object.values(images).map((m) => m.default);
 
     let splash = -1
+    let mask = ""
+    let text = ""
 
     onMount(async () => {
         splash = Math.floor(Math.random() * imageArray.length)
-
-        //console.log("it is ", imageArray)
+        mask = imageArray[splash].src
+        text = mask.substring(mask.lastIndexOf("/") + 1, mask.lastIndexOf("."))
+        
+        console.log("it is ", imageArray)
     })
+
+    function change() {
+        splash++
+        if (splash == imageArray.length)
+            splash = 0
+
+        mask = imageArray[splash].src
+        text = mask.substring(mask.lastIndexOf("/") + 1, mask.lastIndexOf("."))
+        
+    }
 </script>
 
-<div>
-    {#if splash != -1}
-    <img
-        src={imageArray[splash].src}
-        alt=""
-    />
-    {/if}
-</div>
+<button
+    title={text}
+    on:click={change}
+    class="shape block cursor-pointer bg-stone-950/80 mask-no-repeat mask-contain mask-center mask-luminance"
+    style={`
+        mask-image: url('${mask}');
+        -webkit-mask-image: url('${mask}');
+        image-rendering: pixelated;
+        image-rendering: crisp-edges;
+        image-rendering: pixelated;
+    `}
+    >
+</button>
+
+<style lang="postcss">
+    .shape {
+        width: 440px;
+        height: 90px;
+        background-color: var(--accent-color);
+    }
+    
+    .shape:hover {
+        background-color: color-mix(in srgb, var(--accent-color) 80%, transparent);
+    }
+</style>
